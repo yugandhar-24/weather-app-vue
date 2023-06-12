cd8373c1de562e1b13e32cc8bb68bedd34d99a67
yugandhar <yugandhar_goud@trimble.com>
2023-06-12 15:35:44 +0530

    completed app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+      <SiteNavigation />
+      <RouterView v-slot="{Component}">
+        <Transtion name="page" mode="out-in">
+          <component :is="Component"/>
+        </Transtion>
+      </RouterView>
+    </div>
+</template>
+
+<script setup >
+import {RouterView} from "vue-router"
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+<style  scoped>
+
+.page-enter-active,
+.page-leave-active{
+  transition: 600ms ease all;
+}
+
+.page-enter-from,
+.page-leave-to{
+  opacity: 0;
+}
+
+
+</style>
